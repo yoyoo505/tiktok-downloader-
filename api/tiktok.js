a0b56a4d394ac4209b5f87ff994d3d330fb91a79
leyooo <leonardomessisamosir@gmail.com>
2025-10-18 23:35:24 +0700

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ status: false, error: "URL TikTok diperlukan" });
 
-  const api = `https://api-ditss.vercel.app/download/tiktok-v2?apikey=free&url=${encodeURIComponent(url)}`;
+  const api = `https://www.ditss.biz.id/api/download/tiktok-v2?url=${encodeURIComponent(url)}`;
   try {
     const response = await fetch(api);
     const data = await response.json();
